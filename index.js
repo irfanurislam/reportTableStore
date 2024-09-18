7e565c6b58ebb7425e6b3ee6025bd7e0f9b017da
irfanurislam <mdirfanurislam@gmail.com>
2024-09-18 12:20:52 +0600

    first cnnect myql

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const axios = require("axios");
+const mysql = require("mysql2");
 const app = express();
+
 const port = 5000;
+
+// MySQL connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "storeDB", // The database name
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("MySQL connected...");
+});
 
 app.get("/", (req, res) => {
   res.send("This is the Reporting store !");
